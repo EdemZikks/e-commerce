9a0164cd9310289adfb8986dce1657242e9f29ba
EdemZikks <ekdziko@yahoo.com>
2026-05-23 09:43:13 +0100

    refactored test.cy.js

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,21 +1,20 @@
 /// <reference types="cypress" />
+
 import SignUpPage from '../page-object/signUpPage'
 
-  // Store credentials at the describe level
-  // Credentials stored in testCredentials variable (persist across all test)
 
-  //let testCredentials
-
-  // before() method runs and registers the account
-  // Register ONCE before all test
+describe('User Registration', () => {
+  const signUpPage = new SignUpPage()
 
   before(() => {
     cy.visit('/')
 
-
-    // Register the user
-    it('should register a new user', () => {
-      signUpPage.CreateAccount()
-    })
-
+    // Register ONCE before all tests
+    signUpPage.createAccount()
   })
+
+  it('should login successfully', () => {
+    // your test here
+  })
+
+})
